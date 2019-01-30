d48c708c962ce574147b1b92022811fa37b5b6d6
Drozerah <drozerah@drozerah.user>
2019-01-30 11:50:04 +0100

    feat: add 6-reduce.js

---
+++ Functional-Javascript/6-reduce.js
@@ -0,0 +1,24 @@
+// my solution
+const countWords = input => {
+ 
+    let obj = {}
+    let uniq = [...new Set(input)].forEach(elm =>
+        input.reduce((acc, word) => {
+
+            if (word == elm) acc++ 
+            return obj[elm] = acc
+            
+        },0)
+    )
+    return obj
+
+}
+
+// official colution
+
+// function countWords(arr) {
+//     return arr.reduce(function(countMap, word) {
+//       countMap[word] = ++countMap[word] || 1 // increment or initialize to 1        return countMap
+//     }, {}) // second argument to reduce initialises countMap to {}    }
+
+module.exports = countWords
